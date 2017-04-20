5cee35dce1db39b2ca340ca85bb55e3507484a01
piotrmilewski <pmilewski@stuy.edu>
2017-04-19 21:27:20 -0400

    Finished

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -1,16 +1,21 @@
+/*Team Lonely_Island/2: Piotr Milewski, Edwin Mok
+  APCS2 pd3
+  HW32 -- Getting Past the Velvet Rope
+  2017-04-19*/
+
 import java.util.ArrayList;
 
-public class ArrayPriorityQueue<Object>{
+public class ArrayPriorityQueue{
 
     private int _size;
-    private ArrayList<Object> _queue;
+    private ArrayList<Comparable> _queue;
     
     public ArrayPriorityQueue(){
 	_size = 0;
-	_queue = new ArrayList<Object>();
+	_queue = new ArrayList<Comparable>();
     }
     
-    public void add( Object x ){
+    public void add( Comparable x ){
 	if ( isEmpty() ){
 	    _queue.add(x);
 	    _size++;
@@ -18,7 +23,7 @@ public class ArrayPriorityQueue<Object>{
 	else {
 	    //smaller int gets priority
 	    int placeHolder = 0;
-	    while (placeHolder < _size && x.compareTo(_queue.get(placeHolder)) > 0){
+	    while (placeHolder < _size && x.compareTo(_queue.get(placeHolder)) < 0){
 		placeHolder++;
 	    }
 	    _queue.add(placeHolder,x);
@@ -30,23 +35,80 @@ public class ArrayPriorityQueue<Object>{
 	return _size == 0;
     }
 
-    public Object peekMin(){
+    public Comparable peekMin(){
 	if ( isEmpty() ){
 	    throw new IllegalStateException("queue is empty, cannot check from queue");
 	}
 	return _queue.get(_size-1);
     }
 
-    public Object removeMin(){
+    public Comparable removeMin(){
 	if ( isEmpty() ){
 	    throw new IllegalStateException("queue is empty, cannot remove from queue");
 	}
-	Object ret = _queue.remove(_size-1);
+	Comparable ret = _queue.remove(_size-1);
 	_size--;
 	return ret;
     }
 
     public static void main(String[] args){
+	ArrayPriorityQueue a = new ArrayPriorityQueue();
+
+	//Test functionality with strings
+	a.add("c");
+	a.add("b");
+	a.add("d");
+	a.add("a");
+
+	System.out.println(a.isEmpty());
+
+	System.out.println(a.peekMin());
 	
+	System.out.println(a.removeMin());
+	System.out.println(a.removeMin());
+	System.out.println(a.removeMin());
+	System.out.println(a.removeMin());
+
+	System.out.println(a.isEmpty());
+	
+	System.out.println();
+
+	//Test functionality with integers
+	a.add(7);
+	a.add(4);
+	a.add(5);
+	a.add(1);
+
+	System.out.println(a.isEmpty());
+
+	System.out.println(a.peekMin());
+	
+	System.out.println(a.removeMin());
+	System.out.println(a.removeMin());
+	System.out.println(a.removeMin());
+	System.out.println(a.removeMin());
+
+	System.out.println(a.isEmpty());
+	
+	System.out.println();
+
+	//Test functionality with doubles
+	a.add(2.54);
+	a.add(6.94);
+	a.add(3.33);
+	a.add(20.56);
+
+	System.out.println(a.isEmpty());
+
+	System.out.println(a.peekMin());
+	
+	System.out.println(a.removeMin());
+	System.out.println(a.removeMin());
+	System.out.println(a.removeMin());
+	System.out.println(a.removeMin());
+
+	System.out.println(a.isEmpty());
+
+	System.out.println();
     }
 }
